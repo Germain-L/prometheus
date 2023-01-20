5b419eea77e1083be66a16619d8a34406cef029f
Germain <germain.leignel@gmail.com>
2023-01-20 16:18:03 +0100

    documentation

--- client/client.py
+++ client/client.py
@@ -4,6 +4,8 @@ import time
 
 url = "http://server:8000"
 
+
+# Send requests to the server in a loop with a random interval between 1ms and 500ms
 while True:
     time.sleep(random.uniform(0.001, 0.1)) # sleep for a random interval between 1ms and 500ms
     response = requests.get(url)
--- server/server.py
+++ server/server.py
@@ -1,24 +1,36 @@
 from prometheus_client import Summary, Counter, Histogram, generate_latest
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 REQUEST_COUNT = Counter('request_count', 'Total count of requests')
 
+
 class HelloHandler(BaseHTTPRequestHandler):
+    # Decorate function with time metric to track how long it takes to process requests.
     @REQUEST_TIME.time()
     def do_GET(self):
+        # Increment counter for each request made.
         REQUEST_COUNT.inc()
+
+        # if url is '/metrics':
         if self.path == '/metrics':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain; version=0.0.4; charset=utf-8')
             self.end_headers()
+
+            # Generate prometheus metrics.
             self.wfile.write(generate_latest())
+
         else:
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(b'Hello, World!')
 
+# Create a web server and define the handler
 httpd = HTTPServer(('', 8000), HelloHandler)
 print('Server running on port 8000...')
+
+# Wait forever for incoming http requests
 httpd.serve_forever()
