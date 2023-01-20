e959e11a558c18b7a8574dec0198a31f2acc29b8
Germain <germain.leignel@gmail.com>
2023-01-20 13:38:10 +0100

    first commit

---
+++ client/client.py
@@ -0,0 +1,10 @@
+import requests
+import random
+import time
+
+url = "http://server:8000"
+
+while True:
+    time.sleep(random.uniform(0.001, 0.1)) # sleep for a random interval between 1ms and 500ms
+    response = requests.get(url)
+    print(response.text)
