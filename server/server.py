717a58ebeff965bf4b8d25be56ff5d77525845e5
Germain <germain.leignel@gmail.com>
2023-01-20 14:41:48 +0100

    fix

--- server/server.py
+++ server/server.py
@@ -8,19 +8,16 @@ class HelloHandler(BaseHTTPRequestHandler):
     @REQUEST_TIME.time()
     def do_GET(self):
         REQUEST_COUNT.inc()
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Hello, World!')
-
-    def do_GET(self):
         if self.path == '/metrics':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain; version=0.0.4; charset=utf-8')
             self.end_headers()
             self.wfile.write(generate_latest().encode('utf-8'))
         else:
-            super().do_GET()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'Hello, World!')
 
 httpd = HTTPServer(('', 8000), HelloHandler)
 print('Server running on port 8000...')
