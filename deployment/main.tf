3ffd90f7ea61556ceac938d8b29ae7cd40c4674b
Germain Leignel <germain.leignel@gmail.com>
2023-01-30 14:58:29 +0100

    added prometheus image

---
+++ deployment/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "kube-prometheus-stack"
+  location = "westeurope"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "kubeprometheusacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_container_registry_task" "task" {
+  count                 = length(var.images)
+  name                  = "build-task-${var.images[count.index]}"
+  container_registry_id = azurerm_container_registry.acr.id
+  platform {
+    os = "Linux"
+  }
+  docker_step {
+    dockerfile_path      = "Dockerfile"
+    context_path         = "https://github.com/Germain-L/prometheus.git#main:${var.images[count.index]}"
+    context_access_token = var.GITHUB_ACCESS_TOKEN
+    image_names          = ["${var.images[count.index]}:latest"]
+  }
+}
+
+resource "azurerm_container_registry_task_schedule_run_now" "build" {
+  count                      = length(var.images)
+  container_registry_task_id = azurerm_container_registry_task.task[count.index].id
+}
+
---
+++ deployment/variable.tf
@@ -0,0 +1,8 @@
+variable "images" {
+  type    = list(string)
+  default = ["client", "server", "prometheus"]
+}
+
+variable "GITHUB_ACCESS_TOKEN" {
+
+}
